4bdd73e6a71ed96985a2b392807bb7b2e64fbc81
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-10-12 14:54:13 +0300

    chart su spalvom

--- script.js
+++ script.js
@@ -5,25 +5,23 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow"],
+        labels: ["Algis", "Juozas", "Andrius"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3],
-            backgroundColor: [],
-            borderColor: [],
-            borderWidth: 5
-        }]
+            label: 'Amzius',
+            data: [34, 43, 55],
+            borderWidth: 3,
+            backgroundColor: ["red", "red", "blue"],
+        }, {
+            label: 'Alga',
+            data: [340, 830, 550],
+            borderWidth: 3,
+            backgroundColor: ["orange", "green", "green"],
+            type: 'bar',
+                    }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
+    
 });
+
 
 
 function chartAdd() {
@@ -31,15 +29,36 @@ function chartAdd() {
 
     myChart.data.labels.push(document.getElementById('vardas').value);
     myChart.data.datasets[0].data.push(document.getElementById('amzius').value);
-    myChart.update();
+    myChart.data.datasets[1].data.push(document.getElementById('alga').value);
+    
+
+        if (parseInt(document.getElementById('amzius').value) < 50){
+           myChart.data.datasets[0].backgroundColor.push("red");
+    } else {
+            myChart.data.datasets[0].backgroundColor.push("blue");
     }
 
+        if (parseInt(document.getElementById('alga').value) < 380){
+           myChart.data.datasets[1].backgroundColor.push("orange");
+    } else {
+            myChart.data.datasets[1].backgroundColor.push("green");
+    }
+
+    myChart.update();
+}
+
 function chartRemoveLast() {
-   myChart.data.labels.pop();
+    myChart.data.labels.pop();
     myChart.data.datasets[0].data.pop();
     myChart.update();
 }
 
 function chartClear() {
-    myChart.clear();
+    for (var i = myChart.data.labels.length - 1; i >= 0; i--) {
+        myChart.data.labels.pop();
+        myChart.data.datasets[0].data.pop();
+        myChart.update();
+    }
 }
+
+
